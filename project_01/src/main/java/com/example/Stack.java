5520b3e30970c8f2bf1a2ffdb3dcb4b704d619af
Anna Zwiefka <a.zwiefka.657@studms.ug.edu.pl>
2023-03-23 18:05:44 +0100

    added test to check size of stack

--- project_01/src/main/java/com/example/Stack.java
+++ project_01/src/main/java/com/example/Stack.java
@@ -1,25 +1,68 @@
 package com.example;
 
-public class Stack 
-{
-    int stack[] = new int[10];
+public class Stack {
+    private int capacity;
+    private int expandCapacity = 2;
+    private String[] stack;
+   
+    public  Stack (){
+       capacity = 2;
+       this.stack = new String[capacity];
+    
+    }
 
-    public void push(String item) {
-      
+    public Stack(int size) {
+        this.capacity = size;
+        this.stack = new String[capacity];
+       
+    }
+
+    public Stack (int size, int expandCapacity){
+        this.capacity = size;
+        this.expandCapacity = expandCapacity;
+        this.stack = new String[capacity];
     }
 
 
-   public Boolean isEmpty(){
+
+  
+
+
+    public void push(String item) {
+        if (item == null) {
+            throw new IllegalArgumentException();
+        }
+
+        expandStackCapacity();
+    
+
+    }
+
+    private String[] expandStackCapacity() {
+        if (stack.length == capacity) {
+            String newStack[] = new String[capacity + expandCapacity];
+
+            System.arraycopy(stack, 0, newStack, 0, stack.length);
+            stack = newStack;
+        }
+        return stack;
+    }
+
+    public Boolean isEmpty() {
 
         return true;
     }
 
-    public String peek(){
+    public String peek() {
         return null;
     }
 
-    public String pop(){
+    public String pop() {
         return null;
     }
-    
+
+    public int length() {
+        return stack.length;
+    }
+
 }
