860160da23e72edd53c0df288bced1745d70d462
Anna Zwiefka <a.zwiefka.657@studms.ug.edu.pl>
2023-03-23 21:12:28 +0100

    implemented first tests for rpn

---
+++ project_01/src/test/java/com/example/RPNTest.java
@@ -0,0 +1,84 @@
+package com.example;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class RPNTest {
+    @Test
+    void calculateShouldReturnZeroIfExpressionIsEmpty() {
+        final RPN rpn = new RPN();
+        final int result = rpn.calculate(new String[] {});
+        assertTrue(result == 0);
+    }
+
+    @Test
+    void calculateShouldThrowAnExceptionIfCalculatingNull() {
+        final RPN rpn = new RPN();
+        assertThrows(IllegalArgumentException.class, () -> {
+            rpn.calculate(null);
+        });
+    }
+
+    @Test
+    void calculateShouldThrowAnExceptionIfCalculatingNotEnoughOperands() {
+        final RPN rpn = new RPN();
+        assertThrows(IllegalArgumentException.class, () -> {
+            rpn.calculate(new String[] { "1", "2" });
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            rpn.calculate(new String[] { "+", "2" });
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            rpn.calculate(new String[] { "-","-" });
+        });
+    }
+@Test
+void calculateShouldThorwAnExceptionIfInvalidSymbols1(){
+    final RPN rpn = new RPN();
+    assertThrows(IllegalArgumentException.class, () -> {
+        rpn.calculate(new String[] { "1", "a", "3" });
+    });
+
+    assertThrows(IllegalArgumentException.class, () -> {
+        rpn.calculate(new String[] {  "a" });
+    });
+
+    assertThrows(IllegalArgumentException.class, () -> {
+        rpn.calculate(new String[] {  "/" });
+    });
+}
+
+
+@Test 
+void calculateShouldReturnCorrectResultForSimpleExpression() {
+    final RPN rpn = new RPN();
+    final int result = rpn.calculate(new String[] { "1", "2", "+" });
+    assertTrue(result == 3);
+  
+
+}
+@Test 
+void calculateShouldReturnCorrectResultForSimpleExpression2() {
+    final RPN rpn = new RPN();
+    final int result = rpn.calculate(new String[] { "1", "2", "-" });
+    assertTrue(result == -1);
+
+
+}
+
+@Test
+void calculateShouldReturnCorrectResultForSimpleExpression3() {
+    final RPN rpn = new RPN();
+    final int result = rpn.calculate(new String[] { "1", "2", "*" });
+    assertTrue(result == 2);
+
+
+}
+
+
+
+
+}
