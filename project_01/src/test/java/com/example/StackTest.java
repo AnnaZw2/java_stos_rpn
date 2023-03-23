55b94229bef02dc60a75bcdc176e28208d2737ae
Anna Zwiefka <a.zwiefka.657@studms.ug.edu.pl>
2023-03-23 16:32:45 +0100

    more test for push method

--- project_01/src/test/java/com/example/StackTest.java
+++ project_01/src/test/java/com/example/StackTest.java
@@ -1,6 +1,8 @@
 package com.example;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -18,5 +20,33 @@ public class StackTest {
         }
             );
     }
+    @Test
+    void  initializedStackShouldBeEmpty(){
+        final Stack stack = new Stack();
+        assertTrue(stack.isEmpty());
+
+    }
+
+    @Test
+    void elementIsAddedToStackAfterPush(){
+        final Stack stack = new Stack();
+        stack.push("1");
+        assertFalse(stack.isEmpty());
+    }
+
+    @Test
+    void elementInStackIsActuallyElelmentPushed(){
+        final Stack stack = new Stack();
+        stack.push("1");
+        assertFalse(stack.peek().equals("1"));
+    }
+
+    
+    
+   
   
 }
+
+
+  
+
