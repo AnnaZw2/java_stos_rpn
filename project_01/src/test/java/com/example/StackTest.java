88337c9fbc179d36e8c569c667763b968fa64678
Anna Zwiefka <a.zwiefka.657@studms.ug.edu.pl>
2023-03-23 18:34:30 +0100

    added pop tests

--- project_01/src/test/java/com/example/StackTest.java
+++ project_01/src/test/java/com/example/StackTest.java
@@ -6,60 +6,61 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
-
-
 public class StackTest {
+    // Push
     @Test
     void pushShouldThrowExceptionIfArgumentIsNull() {
-       final Stack stack = new Stack();
-        assertThrows(IllegalArgumentException.class, 
-        () -> {
-            stack.push(null);
-        }
-            );
+        final Stack stack = new Stack();
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    stack.push(null);
+                });
     }
+
     @Test
-    void  initializedStackShouldBeEmpty(){
+    void initializedStackShouldBeEmpty() {
         final Stack stack = new Stack();
         assertTrue(stack.isEmpty());
 
     }
 
     @Test
-    void elementIsAddedToStackAfterPush(){
+    void elementIsAddedToStackAfterPush() {
         final Stack stack = new Stack();
         stack.push("1");
         assertFalse(stack.isEmpty());
     }
 
     @Test
-    void elementInStackIsActuallyElelmentPushed(){
+    void elementInStackIsActuallyElelmentPushed() {
         final Stack stack = new Stack();
         stack.push("1");
-        assertFalse(stack.peek().equals("1"));
-    }
-
-    @Test 
-    void stackInitializedWithSpecificLengthShoulHaveThisLength(){
-        final int size = 2;
-        final Stack stack = new Stack(size);
-       assertTrue(stack.length()==size);
-     
+        assertTrue(stack.peek().equals("1"));
     }
 
     @Test
-    void stackShouldExpandCapacityWhenFull(){
+    void stackInitializedWithSpecificLengthShoulHaveThisLength() {
+        final int size = 2;
+        final Stack stack = new Stack(size);
+        assertTrue(stack.length() == size);
+
+    }
+
+    @Test
+    void stackShouldExpandCapacityWhenFull() {
         final int size = 2;
         final int expandCapacity = 5;
         final Stack stack = new Stack(size, expandCapacity);
         stack.push("1");
         stack.push("2");
         stack.push("3");
-        assertTrue(stack.length()==size+expandCapacity);
+        assertTrue(stack.length() == size + expandCapacity);
     }
-    
-    @Test 
-    void peekShouldReturnLastElementPushed(){
+
+    // Peek
+
+    @Test
+    void peekShouldReturnLastElementPushed() {
         final Stack stack = new Stack();
         stack.push("1");
         stack.push("2");
@@ -68,14 +69,74 @@ public class StackTest {
     }
 
     @Test
-    void peekShouldReturnNullIfStackIsEmpty(){
+    void peekShouldReturnNullIfStackIsEmpty() {
         final Stack stack = new Stack();
-        assertTrue(stack.peek()==null);
-   
+        assertTrue(stack.peek() == null);
+
     }
 
+    // IsEmpty
+
+    @Test
+    void isEmptyShouldReturnTrueIfStackIsEmpty() {
+        final Stack stack = new Stack();
+        assertTrue(stack.isEmpty());
+    }
+
+    @Test
+    void isEmptyShouldReturnFalseIfStackIsNotEmpty() {
+        final Stack stack = new Stack();
+        stack.push("1");
+        assertFalse(stack.isEmpty());
+    }
+
+    // Length
+    @Test
+    void lengthShouldReturnLengthOfStack() {
+        final Stack stack = new Stack(3);
+        assertTrue(stack.length() == 3);
+
+    }
+
+    @Test
+    void lengthShouldReturnLengthOfStackAfterExpanding() {
+        final int size = 3;
+        final int expandCapacity = 3;
+        final Stack stack = new Stack(3, 3);
+        stack.push("1");
+        stack.push("2");
+        stack.push("3");
+        stack.push("4");
+        assertTrue(stack.length() == size + expandCapacity);
+
+    }
+
+    //Pop
+    @Test
+    void popShouldThorwExceptionIfStackIsEmpty() {
+        final Stack stack = new Stack();
+        assertThrows(IllegalStateException.class,
+                () -> {
+                    stack.pop();
+                });
+    }
+
+    @Test
+    void popShouldReturnLastElementPushed() {
+        final Stack stack = new Stack();
+        stack.push("1");
+        stack.push("2");
+        stack.push("3");
+        assertTrue(stack.pop().equals("3"));
+    }
+
+    @Test
+    void popShouldRemoveLastElementPushed() {
+        final Stack stack = new Stack();
+        stack.push("1");
+        stack.push("2");
+        stack.push("3");
+        stack.pop();
+        assertTrue(stack.peek().equals("2"));
+    }
 }
-
-
-  
-
