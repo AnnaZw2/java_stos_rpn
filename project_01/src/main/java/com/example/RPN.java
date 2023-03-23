237aba87ca461e59323ddd6e3288d69bd3caa36b
Anna Zwiefka <a.zwiefka.657@studms.ug.edu.pl>
2023-03-23 22:44:42 +0100

    refactored

--- project_01/src/main/java/com/example/RPN.java
+++ project_01/src/main/java/com/example/RPN.java
@@ -1,22 +1,21 @@
 package com.example;
 
 public class RPN {
-   
+
     Stack stack = new Stack();
 
     public int calculate(String[] expression) {
         if (expression == null || expression.length == 0) {
             throw new IllegalArgumentException();
         }
-    
+
         for (int i = 0; i < expression.length; i++) {
-            if (isValidOperator(expression[i]) ) {
+            if (isValidOperator(expression[i])) {
                 stack.push(expression[i]);
-            }else if (expression[i].matches("[0-9]+")){
+            } else if (expression[i].matches("[0-9]+")) {
                 stack.push(expression[i]);
 
-            }
-            else {
+            } else {
                 throw new IllegalArgumentException();
             }
 
@@ -27,15 +26,15 @@ public class RPN {
             String operand2 = stack.pop();
 
             String operand1 = stack.pop();
-          
-            if(!operand1.matches("[0-9]+") || !operand2.matches("[0-9]+") || !isValidOperator(operator) || operator == null){
+
+            if (!operand1.matches("[0-9]+") || !operand2.matches("[0-9]+") || !isValidOperator(operator)
+                    || operator == null) {
                 throw new IllegalArgumentException();
-            }
-            else{
+            } else {
                 int result = mathOperation(operator, operand1, operand2);
                 stack.push(String.valueOf(result));
             }
-           
+
         }
 
         if (stack.size() != 1) {
@@ -44,10 +43,8 @@ public class RPN {
 
         return Integer.parseInt(stack.pop());
 
-        
     }
 
-   
     private boolean isValidOperator(String item) {
         switch (item) {
             case "+":
@@ -60,29 +57,25 @@ public class RPN {
                 return false;
         }
     }
-    
+
     private int mathOperation(String operator, String operand1, String operand2) {
 
-       int operand11 = Integer.parseInt(operand1);
+        int operand11 = Integer.parseInt(operand1);
         int operand22 = Integer.parseInt(operand2);
-        System.out.println(operand11);
-        System.out.println(operand22);
 
         switch (operator) {
             case "+":
-            System.out.println(operand11 + operand22);
+
                 return operand11 + operand22;
             case "-":
-            System.out.println(operand11 - operand22);
+
                 return operand11 - operand22;
             case "*":
-            System.out.println(operand11 * operand22);
+
                 return operand11 * operand22;
             default:
                 throw new IllegalArgumentException();
         }
     }
-
- 
 
 }
