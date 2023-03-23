aee9b372f82437221a9757aa6fe657c1a4343f28
Anna Zwiefka <a.zwiefka.657@studms.ug.edu.pl>
2023-03-23 16:33:25 +0100

    imit required methods

---
+++ project_01/src/main/java/com/example/Stack.java
@@ -0,0 +1,25 @@
+package com.example;
+
+public class Stack 
+{
+    int stack[] = new int[10];
+
+    public void push(String item) {
+      
+    }
+
+
+   public Boolean isEmpty(){
+
+        return true;
+    }
+
+    public String peek(){
+        return null;
+    }
+
+    public String pop(){
+        return null;
+    }
+    
+}
